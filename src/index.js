9d5321a230b5959a751eae58c7f587d080e3ed7b
Marco Cobian <maca9608@gmail.com>
2020-09-13 11:31:59 -0500

    add devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import { counterApp } from "./reducers";
 import { createStore } from 'redux'
 import CounterContainer from "./containers/counter";
